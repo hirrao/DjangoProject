bf238590a13ba3ab61f16b108c0ab668f17e22ec
hirrao <git@hirrao.com>
2025-06-07 04:33:53 +0800

    chore

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -84,3 +84,15 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element(By.TAG_NAME, "body").text
         self.assertNotIn("Buy Flowers", page_text)
         self.assertIn("Buy Milk", page_text)
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size["width"] / 2,
+            512,
+            delta=10,
+        )
+
