3e9d486b90fb4fd88859c3f5901efa5ee73be46d
hirrao <git@hirrao.com>
2025-06-06 20:08:04 +0800

    maybe 4

--- notes/tests/functional_tests.py
+++ notes/tests/functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://127.0.0.1:8000/")
         self.assertIn("To-Do", self.browser.title)
@@ -23,11 +28,16 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
         inputbox.send_keys("Buy Flowers")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.check_for_row_in_list_table("1: Buy Flowers")
 
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn("1: Buy Flowers", [row.text for row in rows])
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
+        inputbox.send_keys("Give a gift to Lisi")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(10)
+
+        self.check_for_row_in_list_table("1: Buy Flowers")
+        self.check_for_row_in_list_table("2: Give a gift to Lisi")
 
         self.fail("Finish the test!")
 
