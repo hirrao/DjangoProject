9624a9fcbe3dc26bbd06a8e8701773355e53ae84
hirrao <git@hirrao.com>
2025-06-06 19:05:32 +0800

    change tests

--- notes/tests/functional_tests.py
+++ notes/tests/functional_tests.py
@@ -1,6 +1,9 @@
+import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -10,9 +13,22 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_start(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://127.0.0.1:8000/")
         self.assertIn("TO-DO", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME,"h1").text
+        self.assertIn("TO-DO", header_text)
+
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
+        inputbox.send_keys("Buy Flowers")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn("1: Buy Flowers", [row.text for row in rows])
+
         self.fail("Finish the test!")
 
 if __name__ == '__main__':
